023907b0b87ec67e8104e71c01be0b180873a663
Holmes <haodev007@gmail.com>
2022-10-05 07:55:19 +0800

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Tailwind CSS + Next.js with TypeScript</title>
+        <meta
+          name="description"
+          content="This is template for Tailwind CSS + Next.js + TypeScript."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-center text-7xl">
+          Using Tailwind CSS + Next.js + TypeScript Like a Pro
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
